0bb069c93ed5d227b7a9fa82aaf47e6915ab0890
Robin <robin.sliet@gmail.com>
2023-03-18 10:01:26 +0530

    day 0

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyHomepage extends StatelessWidget {
+  const MyHomepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int time = 8;
+    String name = "Robin";
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Revsion App"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.navigate_next_outlined),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Revesion is going on it'll take $time hours \n: by $name ",
+          textAlign: TextAlign.right,
+          style: const TextStyle(
+            color: Color.fromARGB(255, 5, 13, 255),
+            fontSize: 20,
+          ),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
