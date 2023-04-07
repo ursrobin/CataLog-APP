80858e91f898aac8d2e2f4740bfbf2d58b1acaab
Robin <robin.sliet@gmail.com>
2023-04-07 17:40:35 +0530

    Day 15 completed Learnings: - Singleton classes - Factory keyword

---
+++ lib/models/cartModel.dart
@@ -0,0 +1,28 @@
+import 'package:r1/models/catalog.dart';
+
+class CartModel {
+  late CatalogModel _catalog;
+
+  // Store ids of each item added to cart
+  final List<int> _itemIds = [];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newcatalog) {
+    _catalog = newcatalog;
+  }
+
+  // Get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  // Get total price
+
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  // Add items
+  void addItem(Item item) => _itemIds.add(item.id);
+
+  // Remove item
+  void removeItem(Item item) => _itemIds.remove(item.id);
+}
